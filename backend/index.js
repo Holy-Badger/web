16f55a4c111348213af90b5fb67e80b96c5d6537
Holy-Badger <Holy-Badger@users.noreply.github.com>
2024-03-11 19:00:14 +0300

    some fix

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const port = 3000
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500/']
+  origin: ['http://127.0.0.1:5500']
 }))
 
 let posts = [{ id: 1, title: 'title_1', body: 'body_1' }]
